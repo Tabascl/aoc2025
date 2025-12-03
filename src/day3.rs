5660bbc45392fcbbc5d9ff272f2a4ab54d5e6b4c
Tabascl <aoc@tabascl.com>
2025-12-03 18:13:13 +0100

    Day 3 Part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,4 @@
+use std::cmp::Reverse;
 use std::fs;
 
 pub fn day3() {
@@ -7,29 +8,39 @@ pub fn day3() {
         .map(|s| s.trim())
         .filter(|s| !s.is_empty());
 
-    let mut sum: u32 = 0;
+    let mut sum: u64 = 0;
 
     for bank in banks {
         let nums: Vec<u32> = bank.chars().filter_map(|c| c.to_digit(10)).collect();
 
-        let mut first = 0;
-        let mut second = 1;
-
         let mut i = 0;
-        while i < nums.len() {
-            if nums[i] > nums[first] && i < nums.len() - 1 {
-                first = i;
-                second = i + 1;
-            }
+        let mut indices: [usize; 12] = [0; 12];
 
-            if nums[i] > nums[second] && i > first {
-                second = i;
-            }
+        let mut start = 0;
+        let mut end = nums.len() - 12 + i;
+
+        let mut result = 0;
+
+        while i < 12 {
+            let sl;
+
+            sl = &nums[start..=end];
+
+            let (index, &max_val) = sl
+                .iter()
+                .enumerate()
+                .min_by_key(|&(_, &v)| Reverse(v))
+                .unwrap();
+            indices[i] = start + index;
 
             i += 1;
+            start = start + index + 1;
+            end = nums.len() - 12 + i;
+
+            result += (max_val as u64) * 10_u64.pow((12 - i) as u32);
         }
 
-        sum += nums[first] * 10 + nums[second];
+        sum += result;
     }
 
     println!("{sum}");
